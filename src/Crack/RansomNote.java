a62786a6a416faebac080e23fa874fde3c6fef6e
Charles Nyong <asikpo.charles@yahoo.com>
2016-12-28 02:01:39 -0500

    Solution to the Ransom Note problem HackerRank

---
+++ src/Crack/RansomNote.java
@@ -0,0 +1,31 @@
+package Crack;
+import java.util.*;
+
+public class RansomNote {
+	
+	
+	public static void main(String[] args) {
+		Scanner inputReader = new Scanner(System.in);
+		int countFoundWordsInMagazine = 0;
+		int lengthOfMagazine = inputReader.nextInt();
+		int lengthOfNote = inputReader.nextInt();
+		inputReader.nextLine(); // escape white space character 
+		String Magazine = inputReader.nextLine();
+		String RansomNote = inputReader.nextLine();
+		StringTokenizer tokens = new StringTokenizer(RansomNote, " ");
+		
+		while(tokens.hasMoreTokens()){
+			if(Magazine.indexOf(tokens.nextToken())!= -1){
+				countFoundWordsInMagazine ++;
+			}
+		}
+		
+		if(countFoundWordsInMagazine != lengthOfNote){
+			System.out.println("No");
+		}
+		else{
+			System.out.println("Yes");
+		}
+	}
+
+}
